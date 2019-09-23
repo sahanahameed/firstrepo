086f0105b7180f5b275545dc3d9923cee372316f
SAHANA <SAHANA@LAPTOP-KCGVKNJ8.mshome.net>
2019-09-23 16:32:01 +0530

    third commit

--- DemoProjGit/src/com/ust/git/Main.java
+++ DemoProjGit/src/com/ust/git/Main.java
@@ -4,12 +4,12 @@ public class Main {
 
 	public static void main (String[] args){
 		SalesData objSD=new SalesData();
-		System.out.println("hello guys");
+		System.out.println("hellooo guys");
 		displayGreetings();
 		objSD.display();
 	}
 	
 	private static void displayGreetings(){
-		System.out.println("welcome to Sales Data");
+		System.out.println("welcome to sales Data");
 	}
 }
